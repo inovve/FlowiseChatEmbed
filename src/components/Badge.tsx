66e949164144ce5d4dd3cbd91b31e9776d10b0c2
Inovve <mail@inovve.com>
2024-03-03 00:21:43 +0000

    Link Update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://turing.pt'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> turing.pt</span>
       </a>
     </span>
   );
